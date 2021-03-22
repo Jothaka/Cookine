d277cb895c1562a5253b91c162bdd0919cb0fc09
Jothaka <jhkahle@hotmail.de>
2021-03-22 19:59:44 +0100

    :sparkles: filter recipe lines for ingredients + tests

---
+++ backend/src/main/java/de/jankahle/capstone/service/RecipeTextFilterService.java
@@ -0,0 +1,97 @@
+package de.jankahle.capstone.service;
+
+import de.jankahle.capstone.model.Ingredient;
+import de.jankahle.capstone.model.Recipe;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.regex.*;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RecipeTextFilterService {
+
+    public Recipe parseStringToRecipe(String recipeString) {
+        return Recipe.builder().build();
+    }
+
+    Optional<Ingredient> parseIngredient(String recipeLine) {
+
+        ArrayList<String> matcherResults = filterLineForIngredientDetails(recipeLine);
+
+        if (matcherResults.isEmpty())
+            return Optional.empty();
+
+        String matcherResult = normalizeMatcherResults(matcherResults);
+
+        String[] ingredientData = matcherResult.split(" ");
+
+        String name = constructIngredientName(ingredientData);
+        String amount = ingredientData[0];
+        String measurementUnit = ingredientData.length > 2 ? ingredientData[1] : "";
+
+        Ingredient parsedIngredient = Ingredient.builder()
+                .name(name)
+                .amount(amount)
+                .measurementUnit(measurementUnit)
+                .build();
+
+        return Optional.of(parsedIngredient);
+    }
+
+    /*
+        <Filter Description>
+        \\d+ we expect an listing of an ingredient starts with a number/the amount of the ingredient this can go in double or triple digits.
+        [\/.,]?\\d? the amount may be partial so we need to consider things like 1/2 or 1.5, this is optional.
+        \\s? depending on the recipe there might be a space between the amount and the measurementUnit or not ("1 kg" or "1kg")
+        [a-zA-Z]* if a measurementUnit is provided it will always be in letters, this is optional as sometimes the measurementUnit is in the ingredients name ("Knoblauchzehe")
+        \\s[A-Z][a-z]+ the ingredient name is expected to start with a capital letter as german nouns always start with one.
+        ([,.]\s[a-zA-Z ]+)? if there is additional information for the ingredient it is usually seperated with ',' or '.' (", mehligkochend") this will be later added to the ingredientname.
+        </Filter Description>
+    */
+    private ArrayList<String> filterLineForIngredientDetails(String recipeLine) {
+        String filter = "(\\d+[\\/.,]?\\d?\\s?[a-zA-Z]*\\s[A-Z][a-z]+([,.]\\s[a-zA-Z ]+)?)";
+        Pattern pattern = Pattern.compile(filter);
+        Matcher matcher = pattern.matcher(recipeLine);
+        ArrayList<String> matcherResults = new ArrayList<>();
+
+        while (matcher.find()) {
+            matcherResults.add(matcher.group());
+        }
+        return matcherResults;
+    }
+
+    private void insertMissingSplitSeperators(StringBuilder matcherResult) {
+        for (int i = 0; i < matcherResult.length() - 1; i++) {
+            if (Character.isDigit(matcherResult.charAt(i))) {
+                if (Character.isAlphabetic(matcherResult.charAt(i + 1))) {
+                    matcherResult.insert(i + 1, ' ');
+                }
+            }
+        }
+    }
+
+    private String normalizeMatcherResults(ArrayList<String> matcherResults) {
+        StringBuilder matcherResult = new StringBuilder(matcherResults.get(0));
+        if (matcherResults.size() > 1) {
+            matcherResult.append(matcherResults.get(1));
+        }
+
+        insertMissingSplitSeperators(matcherResult);
+        return matcherResult.toString();
+    }
+
+    private String constructIngredientName(String[] ingredientData) {
+        StringBuilder nameBuilder = new StringBuilder(ingredientData[ingredientData.length - 1]);
+        if (ingredientData.length > 3) {
+            nameBuilder = new StringBuilder(ingredientData[2]);
+            for (int i = 3; i < ingredientData.length; i++) {
+                nameBuilder.append(" ");
+                nameBuilder.append(ingredientData[i]);
+            }
+        }
+        return nameBuilder.toString();
+    }
+}
---
+++ backend/src/test/java/de/jankahle/capstone/service/RecipeTextFilterServiceTest.java
@@ -0,0 +1,117 @@
+package de.jankahle.capstone.service;
+
+import de.jankahle.capstone.model.Ingredient;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class RecipeTextFilterServiceTest {
+
+    private  String testRecipeString = "a WÜURZIGES KARTOFFELGRATIN\n" +
+            "4 ' ZUTATEN\n" +
+            "a — .\n" +
+            "> BD utspin © 4 ©\n" +
+            "m Da -\n" +
+            "Sn ill a m ir ie ie ir 8 (66) Rezept bewerten Personen\n" +
+            "Pa | -\n" +
+            "BZ .\n" +
+            "z a 1kg Kartoffeln, mehligkochend\n" +
+            "SU) EEE“ DD somn POT sim\n" +
+            "1 MW 4 1 Knoblauchzehe\n" +
+            "en i 400 ml Schlagsahne\n" +
+            "Di 4 ©\n" +
+            "z MS A 100 g Parmesan, gerieben\n" +
+            "X Se f { 50 g Butter\n" +
+            "N 1/2 Bund Petersilie\n" +
+            "etwas Salz\n" +
+            "ZUBEREITUNG\n" +
+            "10 Pr. JS Kartoffel Allrounder\n" +
+            "© Den Backofen auf 180 °C (Umluft) vorheizen. Die Kartoffeln schälen und in ca. 0,5 cm feine\n" +
+            "Scheiben schneiden. Den Knoblauch ebenfalls schälen und halbieren.\n" +
+            "© Die Sahne mit der Creme Fraiche, dem Knoblauch, dem Kartoffel Allrounder und 3 Prisen Salz in\n" +
+            "einem Topf aufkochen lassen und vom Herd nehmen. Den Knoblauch herausnehmen und 50 g\n" +
+            "Parmesan unterheben.\n" +
+            "© Eine Auflaufform gleichmäßig mit der Butter einfetten und die Kartoffelscheiben darin leicht\n" +
+            "überlappend aufschichten, mit der Sahne-Mischung übergießen und mit dem übrigen Parmesan\n" +
+            "bestreuen.\n" +
+            "0 Den Gratin ca. 50 Minuten im Ofen garen, bis es leicht gebräunt ist. Die Petersilie waschen,\n" +
+            "trocken tupfen und fein schneiden. Den Kartoffelgratin mit der Petersilie bestreut servieren.\n";
+
+    @Test
+    void parseStringToRecipeTest() {
+//TODO: write Recipe test
+    }
+
+    @DisplayName("Test Test")
+    @ParameterizedTest
+    @MethodSource
+    void parseIngredientTest(String input, Optional<Ingredient> expected) {
+        //Given
+        RecipeTextFilterService recipeTextFilterService = new RecipeTextFilterService();
+
+        //When
+        Optional<Ingredient> actual = recipeTextFilterService.parseIngredient(input);
+
+        //Then
+        assertThat(actual.isEmpty(), Matchers.is(expected.isEmpty()));
+        if(actual.isPresent() && expected.isPresent()) {
+            assertThat(actual.get(), Matchers.is(expected.get()));
+        }
+    }
+
+    static Stream<Arguments> parseIngredientTest() {
+        String input1 =  "z a 1kg Kartoffeln, mehligkochend";
+        Ingredient expected1 = Ingredient.builder()
+                .name("Kartoffeln, mehligkochend")
+                .amount("1")
+                .measurementUnit("kg")
+                .build();
+
+        String input2 = "1 MW 4 1 Knoblauchzehe";
+        Ingredient expected2 = Ingredient.builder()
+                .name("Knoblauchzehe")
+                .amount("1")
+                .measurementUnit("")
+                .build();
+
+        String input3 = "Scheiben schneiden. Den Knoblauch ebenfalls schälen und halbieren.";
+
+        String input4 = "en i 400 ml Schlagsahne";
+        Ingredient expected4 = Ingredient.builder()
+                .name("Schlagsahne")
+                .amount("400")
+                .measurementUnit("ml")
+                .build();
+
+        String input5 = "N 1/2 Bund Petersilie";
+        Ingredient expected5 = Ingredient.builder()
+                .name("Petersilie")
+                .amount("1/2")
+                .measurementUnit("Bund")
+                .build();
+
+        String input6 = "10 Pr. JS Kartoffel Allrounder";
+        Ingredient expected6 = Ingredient.builder()
+                .name("JS Kartoffel Allrounder")
+                .amount("10")
+                .measurementUnit("Pr.")
+                .build();
+
+        return Stream.of(
+                Arguments.arguments(input1, Optional.of(expected1)),
+                Arguments.arguments(input2, Optional.of(expected2)),
+                Arguments.arguments(input3, Optional.empty()),
+                Arguments.arguments(input4, Optional.of(expected4)),
+                Arguments.arguments(input5, Optional.of(expected5)),
+                Arguments.arguments(input6, Optional.of(expected6))
+        );
+    }
+}
